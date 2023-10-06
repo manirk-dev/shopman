f7969821243a41bae6b4a2150bf88f9b6e629a42
manirk-dev <dev@manirk.com>
2023-10-06 10:29:47 +0300

    fixing check revert logic

---
+++ .github/workflows/actions/check-revert/action.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+try {
+
+    const source = core.getInput("source")
+    const search = core.getInput("search")
+
+    const eventType = github.context.action
+
+    console.log("Event Type: ", eventType)
+
+    var regExp = new RegExp(search, "gi")
+
+    const result = (source.match(regExp) || []).length
+
+    core.setOutput("should-deploy", result === 0 || (result % 2 === 0 && eventType !== "opened"))
+
+} catch (error) {
+    core.setFailed(error.message)
+}
